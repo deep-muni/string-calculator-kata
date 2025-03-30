02e9cc52193089fdee62b107b8f2758c89e44aec
Deep Muni <deep.muni94@gmail.com>
2025-03-30 21:54:09 +0530

    Refactors the methods to handle the default delimiter applied always

--- src/main/java/com/kata/stringcalculator/StringCalculator.java
+++ src/main/java/com/kata/stringcalculator/StringCalculator.java
@@ -13,9 +13,9 @@ public class StringCalculator {
   private static final String NEW_LINE = "\n";
   private static final String MULTIPLICATION = "*";
   private static final String DELIMITER_SEPARATOR = "|";
+  private static final String DEFAULT_DELIMITERS = ",|\n";
   private static final String CUSTOM_DELIMITER_PREFIX = "//";
   private static final String CUSTOM_DELIMITER_REGEX = "[\\[\\]]+";
-  private static final List<String> DEFAULT_DELIMITERS = Arrays.asList(",", NEW_LINE);
   private static final String NEGATIVES_NOT_ALLOWED_MESSAGE = "negatives not allowed - %s";
 
   private static boolean containsCustomDelimiter(String input) {
@@ -27,32 +27,21 @@ public class StringCalculator {
   }
 
   private static boolean shouldMultiply(List<String> delimiters) {
-    boolean shouldMultiply = false;
-
-    if (delimiters.size() == 3 && Pattern.matches(delimiters.getLast(), MULTIPLICATION)) {
-      shouldMultiply = true;
-    }
-    return shouldMultiply;
+    return delimiters.size() == 1 && Pattern.matches(delimiters.getLast(), MULTIPLICATION);
   }
 
   private static List<String> extractDelimiters(String input) {
     if (!containsCustomDelimiter(input)) {
-      return DEFAULT_DELIMITERS;
+      return new ArrayList<>();
     }
 
     int separatorIndex = input.indexOf(NEW_LINE);
     String delimiterPart = input.substring(2, separatorIndex);
 
-    List<String> delimiters = new ArrayList<>(DEFAULT_DELIMITERS);
-    List<String> customDelimiters =
-        Arrays.stream(delimiterPart.split(CUSTOM_DELIMITER_REGEX))
-            .filter(StringUtils::isNotBlank)
-            .map(Pattern::quote)
-            .toList();
-
-    delimiters.addAll(customDelimiters);
-
-    return delimiters;
+    return Arrays.stream(delimiterPart.split(CUSTOM_DELIMITER_REGEX))
+        .filter(StringUtils::isNotBlank)
+        .map(Pattern::quote)
+        .toList();
   }
 
   private static List<Integer> extractValidNumbers(String input, List<String> delimiters) {
@@ -60,7 +49,14 @@ public class StringCalculator {
       int separatorIndex = input.indexOf(NEW_LINE);
       input = input.substring(separatorIndex + 1);
     }
-    return Arrays.stream(input.split(String.join(DELIMITER_SEPARATOR, delimiters)))
+
+    String delimiter = DEFAULT_DELIMITERS;
+
+    if (!delimiters.isEmpty()) {
+      delimiter += DELIMITER_SEPARATOR + String.join(DELIMITER_SEPARATOR, delimiters);
+    }
+
+    return Arrays.stream(input.split(delimiter))
         .map(Integer::parseInt)
         .filter(num -> !isGreaterThanMaximum(num))
         .toList();
