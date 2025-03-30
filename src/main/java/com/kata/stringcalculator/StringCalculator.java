676162e67743c53ce2d398eab2f563e0ea406a7d
Deep Muni <deep.muni94@gmail.com>
2025-03-30 16:03:28 +0530

    Refactors add method to handle single or multiple integers in input string

--- src/main/java/com/kata/stringcalculator/StringCalculator.java
+++ src/main/java/com/kata/stringcalculator/StringCalculator.java
@@ -11,17 +11,6 @@ public class StringCalculator {
       return 0;
     }
 
-    if (input.length() == 1) {
-      return Integer.parseInt(input);
-    }
-
-    if (input.length() == 3) {
-      return Integer.parseInt(String.valueOf(input.charAt(0)))
-          + Integer.parseInt(String.valueOf(input.charAt(2)));
-    }
-
-    return Arrays.stream(input.split(","))
-        .map(num -> Integer.parseInt(num))
-        .reduce(0, (sum, num) -> sum + num);
+    return Arrays.stream(input.split(",")).map(Integer::parseInt).reduce(0, Integer::sum);
   }
 }
